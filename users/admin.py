e195c7653078ebcb44c5253ac0ca4731edc0f77b
Jung <jungeun.lak@gmail.com>
2020-01-17 23:12:15 +0900

    django admin을 다루는 방법

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 from . import models
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     """ Custom User Admin """
 
-    list_display = ("username", "gender", "language", "currency", "superhost")
-    list_filter = (
-        "currency",
-        "superhost",
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
     )
-
